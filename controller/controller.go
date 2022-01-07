099ce43de079eaff5b66baf7f0d269ec7e0ec778
Maheswari1992 <maheswari214@gmail.com>
2022-01-07 13:17:58 +0530

    adding files

--- controller/controller.go
+++ controller/controller.go
@@ -9,11 +9,9 @@ import (
 var (
 	db          *gorm.DB
 	userWrapper *wrapper.UserWrapper
-	shopWrapper *wrapper.ShopWrapper
 )
 
 func InitializeController(DB *gorm.DB) {
 	db = DB
 	userWrapper = wrapper.CreateUserWrapper(db)
-	shopWrapper = wrapper.CreateShopWrapper(db)
 }
